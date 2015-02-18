bc762f8a84f7a6fa63c6f491ba3a429d4986806d
Philippe Schwyter <pschwyter90@gmail.com>
2015-02-18 18:40:59 -0500

    Added titles to static_pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,7 +16,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "about", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
